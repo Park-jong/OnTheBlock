bdbcab6ee176d8945dba5414c68180cbac67be67
Jongwon97 <whddnjs3214@naver.com>
2023-09-05 09:21:17 +0900

    feat: backend/MemberInit
    Former-commit-id: 5bd96e1c5714b9bce4dab0025a7e6dc874bd2b2d

---
+++ Backend/src/main/java/com/ontheblock/www/member/service/MemberServiceImpl.java
@@ -0,0 +1,10 @@
+package com.ontheblock.www.member.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class MemberServiceImpl implements MemberService{
+
+}
