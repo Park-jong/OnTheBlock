39a0340bde1733014704c01bf50c5ac3eb2349b8
seojoonhee <seojoonhee10@gmail.com>
2023-09-05 10:45:37 +0900

    fix:idMusicGenre를 Genre로 수정
    Former-commit-id: 7fa6af35485225f620a5c5a4f242f4d06abdd3b5

--- Backend/src/main/java/com/ontheblock/www/genre/repository/GenreRepository.java
+++ Backend/src/main/java/com/ontheblock/www/genre/repository/GenreRepository.java
@@ -3,10 +3,10 @@ package com.ontheblock.www.genre.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ontheblock.www.genre.domain.MusicGenre;
+import com.ontheblock.www.genre.domain.Genre;
 
 @Repository
-public interface MusicGenreRepository extends JpaRepository<MusicGenre, Integer> {
+public interface GenreRepository extends JpaRepository<Genre, Integer> {
 
 
 }
