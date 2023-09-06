aae45403d309537915b0fd3b1ce5a49a5006cb1d
seojoonhee <seojoonhee10@gmail.com>
2023-09-06 14:57:15 +0900

    feat:membernotice service and controller
    Former-commit-id: db45d397782b18ae9e03996f6434378bebafd316

--- Backend/src/main/java/com/ontheblock/www/notice/repository/MemberNoticeRepository.java
+++ Backend/src/main/java/com/ontheblock/www/notice/repository/MemberNoticeRepository.java
@@ -1,12 +1,15 @@
 package com.ontheblock.www.notice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ontheblock.www.member.Member;
 import com.ontheblock.www.notice.domain.MemberNotice;
 
 @Repository
 public interface MemberNoticeRepository extends JpaRepository<MemberNotice, Long> {
-
+	List<MemberNotice> findByMember(Member member);
 
 }
