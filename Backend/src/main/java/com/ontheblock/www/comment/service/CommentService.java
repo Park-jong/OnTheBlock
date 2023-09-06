c45da8961110319dc5d2368c7725ffe7b93fce7a
jongseong <whd1707@gmail.com>
2023-09-06 13:40:43 +0900

    feat: comment service, repository 개발
    Former-commit-id: 71821ae4664501a471331662eee0c259a69861a7

--- Backend/src/main/java/com/ontheblock/www/comment/service/CommentService.java
+++ Backend/src/main/java/com/ontheblock/www/comment/service/CommentService.java
@@ -1,5 +1,3 @@
-package com.ontheblock.www.comment.service;
-
-public class CommentService {
-    
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e12241d14a785c40703b23bb188c18683e428397de027fe1305d6dc721ac02a
+size 2440
