4e8ed226467a920269603d1f6b17b8afe28a4576
gyqls234 <gyqls234@naver.com>
2023-09-08 16:54:31 +0900

    feat: record + composition on progress
    Former-commit-id: 0cda75a8b8449c2134d348c936c336bcc0d24c1b

--- Frontend/src/pages/record/Record.jsx
+++ Frontend/src/pages/record/Record.jsx
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9ef3440c3536a84115c9562e52c59a7e3d31d7962accb1e4eb8131f15f9180a5
-size 1632
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { RecordComponent } from "@/components";
+
+function Record() {
+  const navigate = useNavigate();
+
+  return (
+    <S.Wrap>
+      <RecordComponent />
+    </S.Wrap>
+  );
+}
+
+const S = {
+  Wrap: styled.div`
+    min-height: 100vh;
+    background: #252525;
+    padding: 0px;
+    color: #d7d7d7;
+  `,
+};
+
+export default Record;
