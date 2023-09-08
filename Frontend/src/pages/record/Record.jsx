08db8324fad9895b2776e34d4a74416365498c53
gyqls234 <gyqls234@naver.com>
2023-09-08 11:19:41 +0900

    media api added
    Former-commit-id: da9f1c5382f50c5e6cacf1a54bbdd1c4f9656fd4

--- Frontend/src/pages/record/Record.jsx
+++ Frontend/src/pages/record/Record.jsx
@@ -1,4 +1,3 @@
-import React from "react";
-
-
-// https://codesandbox.io/s/video-4stru
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ef3440c3536a84115c9562e52c59a7e3d31d7962accb1e4eb8131f15f9180a5
+size 1632
