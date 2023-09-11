6398521dc5721abe27a4c0de9ddb3de26865ebac
Jongwon97 <whddnjs3214@naver.com>
2023-09-11 14:27:11 +0900

    feat: newMemberCheck
    Former-commit-id: 32112ceedb59e5834a2b42e79f7441d3fa3ad350

--- Backend/src/main/java/com/ontheblock/www/member/service/MemberService.java
+++ Backend/src/main/java/com/ontheblock/www/member/service/MemberService.java
@@ -3,7 +3,7 @@ package com.ontheblock.www.member.service;
 import com.ontheblock.www.member.Member;
 
 public interface MemberService {
-    public void saveRefreshToken(Member member);	                    // 토큰 저장
+    public void saveRefreshToken(Long memberId, String refreshToken);	                    // 토큰 저장
     public String getRefreshToken(Long id);								// 토큰 반환
     public void deleteRefreshToken(Long userId);                        // 토큰 삭제
 }
