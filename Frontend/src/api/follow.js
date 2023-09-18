1b5e312f429dd7578549b81992da47c14e520b21
seojoonhee <seojoonhee10@gmail.com>
2023-09-18 14:36:14 +0900

    feat: profile info 추가, follow following 목록 요청응답출력
    Former-commit-id: e82dbc5bfc2e26398958f166979a05fb0d61088a

--- Frontend/src/api/follow.js
+++ Frontend/src/api/follow.js
@@ -6,6 +6,6 @@ export const getFollower=()=>{
     return clientWithToken().get(followURL + 'member/follower/check');
 }
 
-// export const getFollowing=()=>{
-//     return clientWithToken().get(followURL + 'member/follower/check');
-// }
+export const getFollowing=()=>{
+    return clientWithToken().get(followURL + 'member/following/check');
+}
