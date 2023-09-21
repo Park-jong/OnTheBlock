28221de7a65d5150f5d1905ebb15b7d1f6d63e93
seojoonhee <seojoonhee10@gmail.com>
2023-09-21 14:19:17 +0900

    feat: genre api랑 memberinit 페이지 일부
    Former-commit-id: c74aaf5ce2ea94c030fceccb618edb42b2b78f3d

--- Frontend/src/api/instrument.js
+++ Frontend/src/api/instrument.js
@@ -4,12 +4,12 @@ const InstrumentURL = 'instrument/';
 
 export const getAllInstruments = () => {
     return client().get(InstrumentURL + 'findAll');
-  };
+};
 
 export const registMemberInstruments=(selectedInstruments)=>{
     return clientWithToken().post(InstrumentURL+'member/check',selectedInstruments);
-}
+};
 
 export const getMyInstruments=()=>{
   return clientWithToken().get(InstrumentURL+'get/member/check');
-}
+};
