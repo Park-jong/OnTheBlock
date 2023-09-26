063585460a9b8077d44abd4be66e30d66cecf072
jongseong <whd1707@gmail.com>
2023-09-26 17:42:33 +0900

    fix: http -> https로 수정
    Former-commit-id: 541dc17c476879ed4e99bde3f4ff668bc0cb1186

--- Frontend/src/components/layout/UserDropPanel.jsx
+++ Frontend/src/components/layout/UserDropPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c0ac780b80f99807d084e07be3f0539eb497650c75c38380f4eedae2f601dc4
-size 5775
+oid sha256:ca1c8bdb07d545a362ebf873601086b850d54c8f2c94db29a9bcd3bda0b91317
+size 6048
