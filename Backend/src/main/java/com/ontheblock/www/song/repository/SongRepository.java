c3b86ddb97d6e08ce8088b3e0c8dfa78acb3b285
seojoonhee <seojoonhee10@gmail.com>
2023-09-27 14:01:54 +0900

    feat: 메인페이지 곡 검색
    Former-commit-id: f6ccf8d2ab7b0c1a644e0481e19c83915521b4f2

--- Backend/src/main/java/com/ontheblock/www/song/repository/SongRepository.java
+++ Backend/src/main/java/com/ontheblock/www/song/repository/SongRepository.java
@@ -1,8 +1,11 @@
 package com.ontheblock.www.song.repository;
 
+import java.util.List;
+
 import com.ontheblock.www.song.domain.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
+	List<Song> findByName(String name);
 
 }
