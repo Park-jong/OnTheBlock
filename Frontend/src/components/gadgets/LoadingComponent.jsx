c2053651f0e675139d16d57c69f442b7a32b964d
gyqls234 <gyqls234@naver.com>
2023-09-27 15:49:59 +0900

    feat: composition volume saved to DB
    Former-commit-id: f182472561d923ccb0e21a75b4ff18c11df99cc9

--- Frontend/src/components/gadgets/LoadingComponent.jsx
+++ Frontend/src/components/gadgets/LoadingComponent.jsx
@@ -1,33 +1,3 @@
-import styled from "styled-components";
-
-function LoadingComponent() {
-  
-  return (
-    <S.Ripple class="ld-ripple">
-      <div></div>
-      <div></div>
-    </S.Ripple>
-  );
-}
-
-const S = {
-  Ripple: styled.div`
-      position: relative;
-      width: 80px;
-      height: 80px;
-
-
-    > div {
-      position: absolute;
-      border: 4px solid #fff;
-      opacity: 1;
-      border-radius: 50%;
-      animation: ld-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
-    }
-  `,
-
-  
-};
-
-
-export default LoadingComponent;
+version https://git-lfs.github.com/spec/v1
+oid sha256:25f02f7477187bc03b0cabd2d55f1fb2f34b8a37e45496a15d413e8611de6a0e
+size 516
