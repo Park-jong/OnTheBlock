79a487fe42dbf0b31cb3bf46d9c6db857095e8f6
박종성 <whd1707@gmail.com>
2023-10-09 21:36:05 +0900

    fix: 삭제 test
    Former-commit-id: 86108796846a320cb10083deeaac8219c99af3bf

--- Backend/src/main/java/com/ontheblock/www/video/service/VideoService.java
+++ Backend/src/main/java/com/ontheblock/www/video/service/VideoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8391477efeba9cb0613f8ba15b725c4fbac0283cd64a1ef13c99d8be38a6e166
-size 10318
+oid sha256:aed7b2b6a4d7373bc2b59941618ed07da734750a89ee0b33f4115f58dde8759a
+size 10325
